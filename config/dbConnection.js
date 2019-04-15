969a9beeddf045fd2664e3f852d21c30f275dfca
mateus9785 <42577380+mateus9785@users.noreply.github.com>
2019-04-15 12:00:34 -0300

    Add files via upload

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -14,3 +14,17 @@ module.exports = function(){
     console.log('A conecção com o módulo foi estabelecida');
     return connMysql;
 }
+
+
+// var Client=require('pg').Client;
+ 
+// const client = new Client({
+//         host : process.env.PGHOST || "ec2-50-19-127-115.compute-1.amazonaws.com",
+//         user : process.env.PGUSER || "hosvkwkbrddrcp",
+//         password : process.env.PGPASSWORD || "3fa65a5c58f711e14894ca31ce3a7ec9641b7321761091ba707a36c01d5fb28a",
+//         database : process.env.PGDATABASE || "d72g724h506vhi"
+//     });
+
+// client.connect()
+// .then(() => { console.log('connected')})
+// .catch((err) => {console.log(err)});
