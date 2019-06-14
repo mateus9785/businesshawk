51c39b00550d3c60c204d9425d04f5b7f648ecec
mateus9785 <mateusoliveira9785@gmail.com>
2019-06-13 21:07:51 -0300

    alteraçoes finais

--- app/public/js/calculadora.js
+++ app/public/js/calculadora.js
@@ -21,17 +21,18 @@ app.controller('myCtrl1', function($scope) {
     return (((($scope.b-$scope.a)**2)/12)**0.5).toFixed(2);
   };
 });
+
 app.controller('myCtrl2', function($scope) {
-$scope.n=0;
-$scope.p=0;
-$scope.q=0;
-$scope.k=0;
-$scope.binomial = function() {
-    return ((fat($scope.n)/(fat($scope.k)*fat($scope.n-$scope.k)))*
-    (($scope.p/100)**$scope.k)*
-    (($scope.q/100)**($scope.n-$scope.k)))*100;
-};
+  $scope.n=0;
+  $scope.p=0;
+  $scope.q=0;
+  $scope.menor=1;
+  $scope.maior=10;
+  $scope.resultadoProbabilidade = function() {
+    return BinomialSomaResultados($scope.n,$scope.p,$scope.q,$scope.menor,$scope.maior)
+  };
 });
+
 app.controller('myCtrl3', function($scope) {
   $scope.med=0;
   $scope.dp=1;
@@ -40,9 +41,9 @@ app.controller('myCtrl3', function($scope) {
   $scope.maiorEnt=1;
   $scope.menorEnt=0.5;
   $scope.probabilidade1 = function(variavel) {
-      return tipo1(mudaNormal(variavel,$scope.med,$scope.dp));
+    return tipo1(mudaNormal(variavel,$scope.med,$scope.dp));
   };
   $scope.probabilidade2 = function(menor,maior) {
-      return tipoGrafico(mudaNormal(menor,$scope.med,$scope.dp),mudaNormal(maior,$scope.med,$scope.dp));
+    return tipoGrafico(mudaNormal(menor,$scope.med,$scope.dp),mudaNormal(maior,$scope.med,$scope.dp));
   };
 });
